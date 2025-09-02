284bed741d774ff55e6a8fcbe0d5ee315cc98374
Asif Sorathiya <asifiqbalsorathiya@gamilcom>
2025-09-02 22:59:06 +0300

    first chapter

---
+++ scriptfolder/helloworld.js
@@ -0,0 +1,7 @@
+let country="India";
+let continent="Asia";
+let population=13000000000;
+
+console.log(`I live in ${country}, situated in ${continent} continent which has a population of ${population}`);
+// console.log(continent);
+// console.log(population);
