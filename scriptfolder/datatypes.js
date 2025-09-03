857dc9a100a6d780ca2fa080e5790afd0d1d4050
Asif Sorathiya <asifiqbalsorathiya@gamilcom>
2025-09-03 21:58:07 +0300

    data types and letvarconst lecture

---
+++ scriptfolder/datatypes.js
@@ -0,0 +1,32 @@
+//number
+let valueNum=26;
+console.log(valueNum);
+console.log(typeof valueNum);
+
+//string
+let valueString="Asif";
+console.log(valueString);
+console.log(typeof valueString);
+
+//boolean
+let valueBooelan=true;
+console.log(valueBooelan);
+console.log(typeof valueBooelan);
+
+//undefined
+let valueUndefined;
+console.log(valueUndefined);
+console.log(typeof valueUndefined);
+
+
+//null
+
+console.log(null);
+console.log(typeof null);
+
+//dynamic
+valueString=30;
+console.log("string changed to number as dynamic");
+console.log(valueString);
+
+
---
+++ scriptfolder/letvarconst.js
@@ -0,0 +1,18 @@
+//Let can be changed whenever needed
+let firstName="Asif";
+console.log(firstName);
+firstName="John";
+console.log(firstName);
+
+//constant value
+const PI=3.14;
+
+//error will show here because we are trying to change const
+//PI=6;
+console.log(PI);
+
+//var
+var _vr=6;
+_vr="As";
+console.log(_vr);
+
