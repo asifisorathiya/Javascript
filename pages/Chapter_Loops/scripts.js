bd9ae1971a4a7fd886cc42b4b547f4653c7cddbe
Asif Sorathiya <asifiqbalsorathiya@gamilcom>
2025-09-08 19:19:48 +0300

    live-server changes

--- pages/Chapter_Loops/scripts.js
+++ pages/Chapter_Loops/scripts.js
@@ -2,3 +2,6 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weight for a repitation count ${rep}`);
 }
+
+//changes
+console.log("logs");
--- pages/Chapter_Objects/scripts.js
+++ pages/Chapter_Objects/scripts.js
@@ -1,18 +1,17 @@
-const person={
-    name:'Asif',
-    birthYear:1988,
-    job:'Software engineer',
-    friends:['MubarakcalcAge()Nadeem','Javed','Shahid','Afzal'],
-    //create methocalcAge()   
-    calcAge:function()
-    {
-        //craete a new property age
-        this.age=2025-this.birthYear;
-        return this.age;
-    },
-    getSummary:function(){
-        return `${this.name} is a ${this.calcAge()} year old ${this.job}`;    
-    }
+const person = {
+  name: "Asif",
+  birthYear: 1988,
+  job: "Software engineer",
+  friends: ["MubarakcalcAge()Nadeem", "Javed", "Shahid", "Afzal"],
+  //create methocalcAge()
+  calcAge: function () {
+    //craete a new property age
+    this.age = 2025 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.name} is a ${this.calcAge()} year old ${this.job}`;
+  },
 };
 
 console.log(person);
@@ -23,12 +22,14 @@ console.log(person);
 
 console.log(person.name);
 
-console.log(person['name']);
+console.log(person["name"]);
 
 //challenge
 //get count of friends and best friend is mubarak from the object
 
-console.log(`${person.name} has total ${person.friends.length} and his best friend is ${person.friends[0]}`);
+console.log(
+  `${person.name} has total ${person.friends.length} and his best friend is ${person.friends[0]}`
+);
 
 //call function in the object
 console.log(person.calcAge());
